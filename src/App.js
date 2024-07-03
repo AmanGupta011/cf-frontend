590bef45a0dd1292cea00f324223e2fc81e3e3f0
AmanGupta011 <aman.2003.ag@gmail.com>
2024-07-03 17:35:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, createContext } from "react";
+// import { BrowserRouter as  Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import CopyRight from "./components/CopyRight.jsx";
+import axios from "axios";
+import Navbar from "./components/NavBar.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import ShowStatus from "./pages/ShowStatus.jsx";
+import ShowTicket from "./pages/ShowTicket.jsx";
+import Status from "./pages/Status.jsx";
+import Test from "./pages/Test.jsx";
+
+const UserContext = createContext();
 
 function App() {
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const USER_URL = "http://localhost:8000/api/user";
+      const res = await axios.get(USER_URL, {
+        withCredentials: true,
+      });
+      if (res.data?.name) setUser(res.data);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser }}>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/status" element={<ShowStatus />} />
+          <Route path="/status/:ticketID" exact element={<Status />} />
+          <Route
+            path="/test/:contestID/:problemIndex"
+            exact
+            element={<Test />}
+          />
+          <Route
+            path="/status/ticket/:ticketID"
+            exact
+            element={<ShowTicket />}
+          />
+        </Routes>
+        <CopyRight />
+      </div>
+    </UserContext.Provider>
   );
 }
 
 export default App;
+export { UserContext };
---
+++ src/components/CopyRight.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CopyRight = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0">
+      <div className="max-w-[1200px] mx-auto text-white p-5 bg-slate-600 text-center">
+        <p>© 2024 Copyright: Aman Gupta</p>
+        <p>aman.2003.ag@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default CopyRight;
